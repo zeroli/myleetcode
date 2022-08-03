8573dbc70f87bb61965d388f598fa5cbee709553
zeroli <zeroliql@gmail.com>
2022-08-02 18:23:56 -0700

    modify reverse list internval

--- linked-list/fast-slow-pointer/Reverse a linked list from position m to n.cc
+++ linked-list/fast-slow-pointer/Reverse a linked list from position m to n.cc
@@ -1,5 +1,5 @@
 #include "utils.h"
-#include "internal-util.h"
+#include "linked-list/internal-util.h"
 
 // It is guaranteed that 1 ≤ m ≤ n ≤ length of linked list
 Node* ReverseList(Node* list, int m, int n)
@@ -8,18 +8,20 @@ Node* ReverseList(Node* list, int m, int n)
     Node* beg = list;
 
     int i = 1;
-    int m1 = m;
+    // 走m-1步，到达需要反转的头节点
     while (i < m) {
         i++;
         pprev = beg;
         beg = beg->next;
     }
     Node* end = beg;
+    // 再走n-m-1步，到达需要反转链表的尾节点
     while (i < n) {
         i++;
         end = end->next;
     }
     end = end->next;
+    // reverse区间为[beg, end)
     Node* rlist = ReverseList(beg, end);
 
     // connect with first part
@@ -28,9 +30,7 @@ Node* ReverseList(Node* list, int m, int n)
         rlist = list;
     }
     // connect with 3rd part
-    if (end) {
-        beg->next = end;
-    }
+    beg->next = end;
 
     return rlist;
 }
@@ -40,31 +40,28 @@ int main()
     {
         Node* list = BuildList("2->4->6->8->10");
         int m = 2, n = 4;
-        std::cout << "list built from " << m << " to " << n << ":\n";
-        std::cout << list << "\n";
+        std::cout << "input list " << list << "\n";
 
         list = ReverseList(list, m, n);
-        std::cout << "reversed list: " << list << "\n";
+        std::cout << "reversed list from " << m << " to " << n << ": " << list << "\n";
         DestroyList(list);
     }
     {
         Node* list = BuildList("2->3->7->9->23");
         int m = 1, n = 5;
-        std::cout << "list built from " << m << " to " << n << ":\n";
-        std::cout << list << "\n";
+        std::cout << "input list " << list << "\n";
 
         list = ReverseList(list, m, n);
-        std::cout << "reversed list: " << list << "\n";
+        std::cout << "reversed list from " << m << " to " << n << ": " << list << "\n";
         DestroyList(list);
     }
     {
         Node* list = BuildList("10->20->2->5->9->8");
         int m = 1, n = 5;
-        std::cout << "list built from " << m << " to " << n << ":\n";
-        std::cout << list << "\n";
+        std::cout << "input list " << list << "\n";
 
         list = ReverseList(list, m, n);
-        std::cout << "reversed list: " << list << "\n";
+        std::cout << "reversed list from " << m << " to " << n << ": " << list << "\n";
         DestroyList(list);
     }
 
