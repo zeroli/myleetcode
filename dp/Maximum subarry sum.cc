3d5cc42232b3dcc623a5faab8bd2e1a8598d151b
zeroli <zeroliql@gmail.com>
2022-10-29 20:42:04 -0700

    add comments to max sum subarray

--- dp/Maximum subarry sum.cc
+++ dp/Maximum subarry sum.cc
@@ -5,11 +5,12 @@ std::vector<int> MaxSubArray(const std::vector<int>& arr)
 {
     int n = arr.size();
     // 元素类型：起始位置索引和累加和
+    // dp[i]定义为以索引i结尾的元素最大的连续子数组
     std::vector<std::pair<int, int>> dp(n);
     int maxSum = arr[0];
     int start, end;
     for (int i = 1; i < n; i++) {
-        // 当前有2中选择：
+        // 当前有2种选择：
         // 要么将这个数加入到前面的子数组中
         // 要么单独作为一个子数组的开头
         // 最后选择哪一种要看累加和哪个更大
@@ -39,12 +40,12 @@ int MaxSubArray1(const std::vector<int>& arr)
     int sum = 0;
     int maxSum = 0;
     for (int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
         // 如果前面累加和变成了负数，最好不要将当前数加入前面子数组中
         // 让其单独成为一个子数组得开头
         if (sum < 0) {
             sum = 0;
         }
-        sum += arr[i];
         maxSum = std::max(maxSum, sum);
     }
     return maxSum;
