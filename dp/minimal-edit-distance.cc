13375600507eb880c81262d0beb527d5a6154700
zeroli <zeroliql@gmail.com>
2022-10-30 09:26:59 -0700

    add solution to string edit dp 1d

--- dp/minimal-edit-distance.cc
+++ dp/minimal-edit-distance.cc
@@ -31,12 +31,45 @@ int MinimalEditDist(const std::string& str1, const std::string& str2)
     return dp[m][n];
 }
 
+// 降维到1维
+int MinimalEditDist1(const std::string& str1, const std::string& str2)
+{
+    int m = str1.size(), n = str2.size();
+    std::vector<int> dp(n+1, 0);
+    int tmp1 = 1;  // 从空的str2变成1字符长度的str1，只要一步插入
+    // 从空的str1变成任意长度的str2，需要相应的字符插入
+    for (int i = 0; i <= n; i++) {
+        dp[i] = i;
+    }
+    // 纵轴(行)为str1, 横轴(列)为str2
+    for (int i = 1; i <= m; i++) {
+        /*
+           dp[j-1]   dp[j]       => (old_tmp1) dp[j]
+                     \     |
+           tmp1 -->(cost)                          (tmp1)
+        */
+        for (int j = 1; j <= n; j++) {
+            int cost = 0;
+            if (str1[i-1] == str2[j-1]) {
+                cost = dp[j-1];
+            } else {
+                cost = std::min({dp[j-1], dp[j], tmp1}) + 1;
+            }
+            dp[j-1] = tmp1;
+            tmp1 = cost;
+        }
+        dp[n] = tmp1;
+    }
+    return dp[n];
+}
+
 int main()
 {
     {
         std::string str1 = "abbc";
         std::string str2 = "babb";
         std::cout << MinimalEditDist(str1, str2) << "\n";  // 2
+        std::cout << MinimalEditDist1(str1, str2) << "\n";  // 2
         // 先插入b，再删除c
     }
 }
