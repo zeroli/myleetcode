d1c650496d83e3b3d79da7559da54d88d4f8ba51
zeroli <zeroliql@gmail.com>
2022-10-30 18:11:05 -0700

    add comments

--- sliding-window/Minimum Size Subarray Sum.cc
+++ sliding-window/Minimum Size Subarray Sum.cc
@@ -1,7 +1,7 @@
 #include "utils.h"
 
 /*
-Given an array ofnpositive integers and a positive integers,
+Given an array of n positive integers and a positive integers,
  find the minimal length of a contiguous subarray of which the sum ≥s.
   If there isn't one, return 0 instead.
 
@@ -16,6 +16,7 @@ the subarray
  has the minimal length under the problem constraint.
 */
 
+// 这题目与sliding-window\subarray sum equal to K.cc，思路一样
 int MinSizeSubarr(const std::vector<int>& arr, int target)
 {
     int n = arr.size();
