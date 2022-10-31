fca8725a41e81f6794bd1b2e36f96338eae51bd6
zeroli <zeroliql@gmail.com>
2022-10-30 18:14:31 -0700

    add comments

--- sliding-window/max-fruits-count-2-types.cc
+++ sliding-window/max-fruits-count-2-types.cc
@@ -1,18 +1,21 @@
 #include "utils.h"
 
 // 小于等于2种类型的最大窗口长度
+// 窗口内不同字符个数最多2种, at most K (K=2)
 int MaxFruitsCount2Types(const std::string& fruits)
 {
     std::map<char, int> fruitFreqs;
 
     int maxLen = 0;
     int start = 0;
-    for (int end = 0; end < fruits.size(); end++){
+    for (int end = 0; end < fruits.size(); end++) {
         fruitFreqs[fruits[end]]++;
 
         // <= 2 types, we will continue to calculate its length
         // 窗口内不满足要求了，则缩小窗口左边
         while (fruitFreqs.size() > 2) {
+            // 去除左边的字符，如果=0，说明剩下字符没有重复(以前是1)
+            // 可以从map中安全删除
             if (--fruitFreqs[fruits[start]] == 0) {
                 fruitFreqs.erase(fruits[start]);
             }
